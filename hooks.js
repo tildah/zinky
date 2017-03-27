73ac51cc93202818c078e6662c83794e3a9d69eb
burawi <burawi.ba@gmail.com>
2017-03-27 14:41:06 +0100

    fooll/

--- hooks.js
+++ hooks.js
@@ -1,11 +1,11 @@
 const prefix = "fooll-";
-const session = require(prefix + 'session'),
-const parseUrl = require(prefix + 'parseurl'),
-const logRequest = require(prefix + 'logrequest'),
-const setupResponse = require(prefix + 'setupresponse'),
-const isRequestingFile = require(prefix + 'isrequestingfile'),
-const validateRequest = require(prefix + 'validaterequest'),
-const callAction = require(prefix + 'callaction')
+const session = require(prefix + 'session');
+const parseUrl = require(prefix + 'parseurl');
+const logRequest = require(prefix + 'logrequest');
+const setupResponse = require(prefix + 'setupresponse');
+const isRequestingFile = require(prefix + 'isrequestingfile');
+const validateRequest = require(prefix + 'validaterequest');
+const callAction = require(prefix + 'callaction');
 
 module.exports = [
   session,

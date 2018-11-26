49f348b68995d1ac18640d442e6e9ae99abbfd8e
burawi <burawi.ba@gmail.com>
2018-11-26 14:58:17 +0100

    switch from body-parser to co-body

--- hooks/index.js
+++ hooks/index.js
@@ -7,19 +7,18 @@ const validateRequest = require("./validate-request");
 const callAction = require("./call-action");
 const beforeAction = require("./before-action");
 const moduleHook = require("./module-hook");
-const bodyParser = require('body-parser');
+const parseBody = require("co-body");
 const cookieParser = require('cookie-parser');
 
 module.exports = function (settings) {
   return [
+    cookieParser(),
+    async req => { req.body = await parseBody(req, settings.bodyParserOptions); },
     enhanceResponse,
     enhanceRequest,
     logRequest,
     staticFiles,
     validateRequest,
-    cookieParser(),
-    bodyParser.json(settings.bodyParserOptions),
-    bodyParser.urlencoded({ extended: false }),
     moduleHook,
     beforeAction,
     callAction

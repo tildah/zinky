e0c7d026a8ae5b808c43fbbda5b95b1fbd67687b
burawi <burawi.ba@gmail.com>
2019-06-18 13:39:38 +0100

    fix allow cors

--- hooks/allow-cors.js
+++ hooks/allow-cors.js
@@ -1,6 +1,6 @@
 module.exports = async (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "*");
-  res.setHeader("Access-Control-Allow-Headers", "*");
+  res.setHeader("Access-Control-Allow-Headers", req.A.cors.headers.join(","));
   if (req.method == "OPTIONS") return "Cors Allowed";
 };

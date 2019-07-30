e92ef840531c8e00bf67bc9750153c6b7b629f84
burawi <burawi.ba@gmail.com>
2019-07-30 11:32:07 +0100

    add origin to cors params

--- hooks/allow-cors.js
+++ hooks/allow-cors.js
@@ -1,5 +1,5 @@
 module.exports = async (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", req.A.cors.origins.join(","));
   res.setHeader("Access-Control-Allow-Methods", "*");
   res.setHeader("Access-Control-Allow-Headers", req.A.cors.headers.join(","));
   if (req.method == "OPTIONS") return "Cors Allowed";

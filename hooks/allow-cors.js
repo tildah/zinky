67cfcb2eaec72c4a0e2bf420feb1d1973145c55e
burawi <burawi.ba@gmail.com>
2019-08-24 10:14:24 +0100

    remove wild card from methods cors

--- hooks/allow-cors.js
+++ hooks/allow-cors.js
@@ -1,7 +1,7 @@
 module.exports = async (req, res) => {
   const { origins, headers, ...additional } = req.A.cors;
   res.setHeader("Access-Control-Allow-Origin", origins.join(","));
-  res.setHeader("Access-Control-Allow-Methods", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
   res.setHeader("Access-Control-Allow-Headers", headers.join(","));
   Object.keys(additional).forEach(header => {
     res.setHeader(header, additional[header]);

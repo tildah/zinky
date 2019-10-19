e7f64fa7c6d7c9b247058da93cee675953ff8d76
burawi <burawi.ba@gmail.com>
2019-10-19 11:04:28 +0100

    allow multiple origins'

--- hooks/allow-cors.js
+++ hooks/allow-cors.js
@@ -1,7 +1,11 @@
 module.exports = async (req, res) => {
   if (!req.A.allowCors) return;
   const { origins, headers, ...additional } = req.A.cors;
-  res.setHeader("Access-Control-Allow-Origin", origins.join(","));
+  const { origin: clientHost } = req.headers;
+  const allowAll = origins.includes("*");
+  const allowed = allowAll || origins.includes(clientHost);
+  const origin = allowed ? clientHost : "";
+  res.setHeader("Access-Control-Allow-Origin", origin);
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
   res.setHeader("Access-Control-Allow-Headers", headers.join(","));
   Object.keys(additional).forEach(header => {
